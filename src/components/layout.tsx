ba76d11a7725c70db60052b1f513e08f8d0902d2
Mikita Tserakhau <exymore@yandex.by>
2023-06-14 16:24:18 +0300

    chore: set page title

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,4 +1,5 @@
 import { Col, Grid } from '@tremor/react';
+import Head from 'next/head';
 import React from 'react';
 
 import Sidebar from '../modules/Sidebar';
@@ -11,13 +12,19 @@ export default function RootLayout({
   children,
 }: RootLayoutProps): React.JSX.Element {
   return (
-    <Grid numColsSm={1} numColsMd={12} className="h-screen">
-      <Col numColSpanMd={2}>
-        <Sidebar />
-      </Col>
-      <Col numColSpanMd={10}>
-        <div className="px-6 py-6 min-w-fit h-full">{children}</div>
-      </Col>
-    </Grid>
+    <>
+      <Head>
+        <title>Firestorm</title>
+        <meta name="description" content="Financial monitor app" />
+      </Head>
+      <Grid numColsSm={1} numColsMd={12} className="h-screen">
+        <Col numColSpanMd={2}>
+          <Sidebar />
+        </Col>
+        <Col numColSpanMd={10}>
+          <div className="px-6 py-6 min-w-fit h-full">{children}</div>
+        </Col>
+      </Grid>
+    </>
   );
 }
